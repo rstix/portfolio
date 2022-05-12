f7acb281ff65a69da6e91a468170452791c6e18c
Roman Stix <rsonroad@gmail.com>
2022-05-12 01:42:59 -0600

    navbar, home, post done

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <img src="" alt="" />
+      <section className='relative flex justify-content min-h-screen pt-12 '>
+        <h1>
+          Hey! I'm Roman.
+        </h1>
+      </section>
+    </main>
+  )
+}
+
+export default Home;
---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React,{useEffect,useState} from "react";
+import sanityClient from '../client'
+
+
+export default function Projects() {
+
+
+  return (
+
+  )
+}
