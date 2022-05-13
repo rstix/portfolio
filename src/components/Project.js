58c2e07e10b91ee49f12b85a0ebac46991430951
Roman Stix <rsonroad@gmail.com>
2022-05-13 13:38:04 -0600

    date removed from projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -32,19 +32,19 @@ export default function Projects() {
               <a href={project.link} alt={project.title} target='_blank' rel="noreferrer noopener">{project.title}</a>
             </h3>
             <div className='text-gray-500 text-xs space-x-4'> 
-              <span>
+              {/* <span>
                 <strong className='font-bold'>Finished on</strong>: {" "}
                 {new Date(project.data).toLocaleDateString}
-              </span>
-              <span>
+              </span> */}
+              {/* <span>
                 <strong className='font-bold'>Company</strong>:{" "}
                 {project.place}
-              </span>
+              </span> */}
               <span>
                 <strong className='font-bold'>Type</strong>:{" "}
                 {project.projectType}
               </span>
-              <p className='my-6 text-lg text-gray-700 leading-relaxed'></p>
+                <p className='my-6 text-lg text-gray-700 leading-relaxed'>{ project.description}</p>
               <a href={project.link} target='_blank' rel="noreferrer noopener" className='text-red-500 font-bold hover:underline hover:text-red-400'>
                 View The Project{" "}
                 <span role='img' aria-label='right pointer'>👉</span>
